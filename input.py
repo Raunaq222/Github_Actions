7bb78ba57e2d2d4d5ef4551d5de50c7578c6b2bd
Raunaq Bhogal <92616402+Raunaq222@users.noreply.github.com>
2023-04-21 10:11:19 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+# a = input(" Enter your name ")
+# a = int(a) # conv string to int if possible
+# print(type(a),a)
+
+a = input("Enter number 1:")
+b = input("Enter number 2:")
+a = int(a)
+b = int(b)
+avg = (a+b)/2
+print(avg)
